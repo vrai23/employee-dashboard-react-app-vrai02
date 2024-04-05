17f248554bc6619063cd4db7557b7388efc810bb
Vachan Rai <rai.vachan@yahoo.com>
2024-04-05 18:56:00 -0400

    4-5-24 React App

---
+++ src/components/title.jsx
@@ -0,0 +1,9 @@
+import React from 'react' ;
+
+const Title = (props) => {
+    return (
+        <h1>Name: {props.name} Last Name: {props.last}</h1>
+    )
+}
+
+export default Title;
---
+++ src/data/data.js
@@ -0,0 +1,66 @@
+const data = [{
+  "id": "E1002",
+  "firstName": "John",
+  "lastName": "Smith",
+  "email": "john.smith@example.com",
+  "department": "UX/UI Design",
+  "position": "UI Designer",
+  "employmentType": "Part-Time",
+  "skills": ["Sketch", "Figma", "Adobe XD"],
+  "hoursLoggedThisWeek": 20,
+  "onLeave": false
+},
+
+{
+  "id": "E1003",
+  "firstName": "Lisa",
+  "lastName": "White",
+  "email": "lisa.white@example.com",
+  "department": "Marketing",
+  "position": "Digital Marketing Specialist",
+  "employmentType": "Remote",
+  "skills": ["SEO", "Content Marketing", "Google Analytics"],
+  "hoursLoggedThisWeek": 35,
+  "onLeave": false
+},
+
+{
+  "id": "E1004",
+  "firstName": "Mark",
+  "lastName": "Brown",
+  "email": "mark.brown@example.com",
+  "department": "Software Development",
+  "position": "Backend Developer",
+  "employmentType": "Full-Time",
+  "skills": ["Node.js", "MongoDB", "Express"],
+  "hoursLoggedThisWeek": 40,
+  "onLeave": true
+},
+
+{
+  "id": "E1005",
+  "firstName": "Emily",
+  "lastName": "Green",
+  "email": "emily.green@example.com",
+  "department": "Human Resources",
+  "position": "HR Manager",
+  "employmentType": "Full-Time",
+  "skills": ["Recruitment", "Employee Relations", "Performance Management"],
+  "hoursLoggedThisWeek": 40,
+  "onLeave": false
+},
+
+{
+  "id": "E1006",
+  "firstName": "Alex",
+  "lastName": "Johnson",
+  "email": "alex.johnson@example.com",
+  "department": "IT Support",
+  "position": "IT Support Specialist",
+  "employmentType": "Full-Time",
+  "skills": ["Network Security", "Hardware Troubleshooting", "Customer Service"],
+  "hoursLoggedThisWeek": 40,
+  "onLeave": false
+  }]
+
+export default data;
